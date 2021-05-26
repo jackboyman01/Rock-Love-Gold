3c7a8ea9f99459b198772177ea0498b1b4bbe967
jackboyman <jackboyman@web.de>
2021-05-26 15:42:50 +0200

    innit
    first upload

---
+++ Scenes/World.gd
@@ -0,0 +1,41 @@
+extends Node2D
+
+var dialogbox_scene = preload("res://Mini Scenes/Dialog Box.tscn")
+var dialogbox
+var dialogbox_num = 0
+
+func _process(_delta):
+	if dialogbox_num == 1:
+		$Player.MAX_SPEED = 0
+	else:
+		$Player.MAX_SPEED = 500
+	if Input.is_action_pressed("ui_cancel"):
+		if dialogbox_num == 1:
+			$CanvasLayer.get_child(0).hide()
+		$Player/FadeIn.show()
+		$Player/FadeIn.fade_in()
+
+func _ready():
+	MusicPlayer.play_game_music()
+	dialogbox_num += 1
+	dialogbox = dialogbox_scene.instance()
+	dialogbox.dialogpath = "res://Text/Intro.json"
+	$CanvasLayer.add_child(dialogbox)
+
+func _on_Player_Gold():
+	if dialogbox_num != 1:
+		dialogbox_num += 1
+		dialogbox = dialogbox_scene.instance()
+		dialogbox.dialogpath = "res://Text/Gold.json"
+		$CanvasLayer.add_child(dialogbox)
+
+func _on_Player_Rock():
+	if dialogbox_num != 1:
+		dialogbox_num += 1
+		dialogbox = dialogbox_scene.instance()
+		dialogbox.dialogpath = "res://Text/Rock.json"
+		$CanvasLayer.add_child(dialogbox)
+
+func _on_FadeIn_fade_finished():
+# warning-ignore:return_value_discarded
+	get_tree().change_scene("res://Menu/Main Menu.tscn")
---
+++ Scripts/Dialog Box.gd
@@ -0,0 +1,68 @@
+extends ColorRect
+
+export var dialogpath = ""
+export(float) var textspeed = 0.05
+
+var dialog
+
+var phrasenum = 0
+var finished = false
+
+func _ready():
+	$Timer.wait_time = textspeed
+	dialog = getDialog()
+	assert(dialog, "Dialog not found")
+	nextPhrase()
+
+func _process(_delta):
+	$Indicator.visible = finished
+	$Indicator/AnimationPlayer.play("Dialog Box")
+	if Input.is_action_just_pressed("ui_next"):
+		if finished:
+			nextPhrase()
+		else:
+			$Text.visible_characters = len($Text.text)
+
+func getDialog() -> Array:
+	var f = File.new()
+	assert(f.file_exists(dialogpath), "File Path does not exist.")
+
+	f.open(dialogpath, File.READ)
+	var json = f.get_as_text()
+	
+	var output = parse_json(json)
+	
+	if typeof(output) == TYPE_ARRAY:
+		return output
+	else:
+		return []
+
+func nextPhrase() -> void:
+	if phrasenum >= len(dialog):
+		queue_free()
+		get_node("/root/World").dialogbox_num -= 1 
+		return
+	
+	finished = false
+	
+	$Name.bbcode_text = dialog[phrasenum]["Name"]
+	$Text.bbcode_text = dialog[phrasenum]["Text"]
+	
+	$Text.visible_characters = 0
+	
+	var f = File.new()
+	var img = "Text/" + dialog[phrasenum]["Name"] + ".png"
+	if f.file_exists(img):
+		$Potrait.texture = load(img)
+	else:
+		$Potrait.texture = null
+	
+	while $Text.visible_characters < len($Text.text):
+		$Text.visible_characters += 1
+		
+		$Timer.start()
+		yield($Timer, "timeout")
+	
+	finished = true
+	phrasenum += 1
+	return
